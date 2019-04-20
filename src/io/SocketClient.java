8e6dec451874978d988fc93db368d91bd4b06534
renlanchuke <1065730562@qq.com>
2019-04-20 09:52:11 -0700

    socket

---
+++ src/io/SocketClient.java
@@ -0,0 +1,28 @@
+package io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Date;
+
+public class SocketClient {
+	private Socket client;
+
+	public void run(int port) throws InterruptedException {
+		try {
+			client = new Socket("127.0.0.1", port);
+			while (true) {
+				OutputStream out = client.getOutputStream();
+				out.write((new Date()+" Hello world!").getBytes());
+				Thread.sleep(2000);
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		SocketClient sc=new SocketClient();
+		sc.run(18888);
+	}
+}
---
+++ src/io/SocketServer.java
@@ -0,0 +1,39 @@
+package io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+	private ServerSocket serverSocket;
+
+	public void run(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+
+		try {
+			System.out.println("server is starting");
+			
+			Socket socket = serverSocket.accept();
+			byte[] data = new byte[1024];
+			InputStream inputStream = socket.getInputStream();
+
+			while (true) {
+				int len;
+				while((len=inputStream.read(data))!=-1) {
+					System.out.println(new String(data,0,len));
+				}
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		SocketServer ss=new SocketServer();
+		ss.run(18888);
+		
+		
+	}
+}
